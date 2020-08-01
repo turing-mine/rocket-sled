47ce9b00d7acc619c9898c70cf42c1b73158d606
Paul Zabin <pablo.casabas@gmail.com>
2020-08-01 00:18:40 -0700

    adding ts version of views module

--- views/index.js
+++ views/index.js
@@ -1,99 +1,79 @@
-
-var formidable = require('formidable'),
-    fs = require('fs'),
-    util = require('util'),
-    os = require('os')
-    
-var _ = require('underscore')
+var formidable = require('formidable'), fs = require('fs'), util = require('util'), os = require('os');
+var _ = require('underscore');
 _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g
-}
-
-const modID = module.id
-
-const photo = require('../controllers/photo')
-
-exports.xRoot = function(req, res, appRoot, appMeta, serverIpAddr, lPort){
-    fs.readFile(appRoot, 'utf8', function(err, data){
-        if (err){
-            res.writeHead(404, appMeta)
-            return res.end("404 File Not Found")
+};
+var modID = module.id;
+var photo = require('../controllers/photo');
+exports.xRoot = function (req, res, appRoot, appMeta, serverIpAddr, lPort) {
+    fs.readFile(appRoot, 'utf8', function (err, data) {
+        if (err) {
+            res.writeHead(404, appMeta);
+            return res.end("404 File Not Found");
         }
-
-        var rootContent = data
-        var rootTemplate = _.template(rootContent)
-        
-        res.writeHead(200, appMeta)
-        res.write(rootTemplate({ip_addr: serverIpAddr, ip_port: lPort}))
-        res.end()
-    })
-    return
-}
-
-exports.xForm = function(req, res, cheeseRoot, appMeta, serverIpAddr, lPort){
-    fs.readFile(cheeseRoot, 'utf8', function(err, data){
-        if (err){
-            res.writeHead(404, appMeta)
-            return res.end("404 File Not Found")
+        var rootContent = data;
+        var rootTemplate = _.template(rootContent);
+        res.writeHead(200, appMeta);
+        res.write(rootTemplate({ ip_addr: serverIpAddr, ip_port: lPort }));
+        res.end();
+    });
+    return;
+};
+exports.xForm = function (req, res, cheeseRoot, appMeta, serverIpAddr, lPort) {
+    fs.readFile(cheeseRoot, 'utf8', function (err, data) {
+        if (err) {
+            res.writeHead(404, appMeta);
+            return res.end("404 File Not Found");
         }
-
-        var formContent = data
-        var formTemplate = _.template(formContent)
- 
-        res.writeHead(200, appMeta)
-        res.write(formTemplate({ip_addr: serverIpAddr, ip_port: lPort}))
-        res.end()
-    })
-    return
-}
-
-exports.xUpload = function(req, res, buenoRoot, appMeta, serverIpAddr, lPort){
+        var formContent = data;
+        var formTemplate = _.template(formContent);
+        res.writeHead(200, appMeta);
+        res.write(formTemplate({ ip_addr: serverIpAddr, ip_port: lPort }));
+        res.end();
+    });
+    return;
+};
+exports.xUpload = function (req, res, buenoRoot, appMeta, serverIpAddr, lPort) {
     // parse a file upload
-    var form = new formidable.IncomingForm()
-    var imgOrientation
-    var imgType
-    var imgPort
-    if (lPort == "80"){
-        imgPort = ""
+    var form = new formidable.IncomingForm();
+    var imgOrientation;
+    var imgType;
+    var imgPort;
+    if (lPort == "80") {
+        imgPort = "";
     }
-    else{
-        imgPort = ":" + lPort
+    else {
+        imgPort = ":" + lPort;
     }
-
-    form.parse(req, function(err, fields, files){
+    form.parse(req, function (err, fields, files) {
         //res.writeHead(200, {'content-type': 'text/plain'})
         //res.write('received upload:\n\n')
         //res.write(util.inspect({fields: fields, files: files}))
         //console.log(files.upload["type"])
-
-        var retrievalPath = files.upload["path"]
-        var saveName = files.upload["name"]
+        var retrievalPath = files.upload["path"];
+        var saveName = files.upload["name"];
         //console.log("file size: " + files.upload["size"])
         //console.log(fields)
-        var imgOrientation = "360"
-
-        fs.readFile(buenoRoot, 'utf8', function(err, data){
-            if (err){
-                res.writeHead(404, appMeta)
-                return res.end("404 File Not Found")
+        var imgOrientation = "360";
+        fs.readFile(buenoRoot, 'utf8', function (err, data) {
+            if (err) {
+                res.writeHead(404, appMeta);
+                return res.end("404 File Not Found");
             }
- 
-            var uploadContent = data
-            var uploadTemplate = _.template(uploadContent)
-
-            if (files.upload["type"] == 'image/jpeg'){
-                imgType = files.upload.type
-                photo.save(retrievalPath, saveName)
+            var uploadContent = data;
+            var uploadTemplate = _.template(uploadContent);
+            if (files.upload["type"] == 'image/jpeg') {
+                imgType = files.upload.type;
+                photo.save(retrievalPath, saveName);
             }
-            else{
-                imgType = "unknown"
-                console.log('Not an image, no file uploaded...')
+            else {
+                imgType = "unknown";
+                console.log('Not an image, no file uploaded...');
             }
-
-            function showOriented(){
-                console.log(fields)
-                truComment = fields.social
-                res.writeHead(200, appMeta)
+            function showOriented() {
+                console.log(fields);
+                var truComment = fields.social;
+                res.writeHead(200, appMeta);
                 res.write(uploadTemplate({
                     ip_addr: serverIpAddr,
                     ip_port: imgPort,
@@ -102,15 +82,12 @@ exports.xUpload = function(req, res, buenoRoot, appMeta, serverIpAddr, lPort){
                     img_type: imgType,
                     img_orientation: imgOrientation,
                     comments: truComment
-                }))
-                res.end()
+                }));
+                res.end();
                 //console.log(`Image orientation: ${imgOrientation}`)
             }
-    
-            showOriented()
-        })      
-    })
-
-    return
-}
-
+            showOriented();
+        });
+    });
+    return;
+};
